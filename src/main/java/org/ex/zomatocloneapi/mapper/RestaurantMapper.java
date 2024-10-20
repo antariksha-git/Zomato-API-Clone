1e1c08a146b37235c8a28a3d071e1e202b2b9b18
antariksha-git <heyantariksha@gmail.com>
2024-10-20 20:11:21 +0530

    Removed duplicated code

--- src/main/java/org/ex/zomatocloneapi/mapper/RestaurantMapper.java
+++ src/main/java/org/ex/zomatocloneapi/mapper/RestaurantMapper.java
@@ -9,7 +9,6 @@ public class RestaurantMapper {
     public static Restaurant mapToRestaurant(RestaurantRequest restaurantRequest, Restaurant restaurant) {
         restaurant.setDescription(restaurantRequest.getDescription());
         restaurant.setName(restaurantRequest.getName());
-        restaurant.setDescription(restaurantRequest.getDescription());
         restaurant.setEmail(restaurantRequest.getEmail());
         restaurant.setPhoneNumber(restaurantRequest.getPhoneNumber());
         restaurant.setDietType(restaurantRequest.getDietType());
@@ -22,7 +21,6 @@ public class RestaurantMapper {
         restaurantResponse.setRestaurantId(restaurant.getRestaurantId());
         restaurantResponse.setDescription(restaurant.getDescription());
         restaurantResponse.setName(restaurant.getName());
-        restaurantResponse.setDescription(restaurant.getDescription());
         restaurantResponse.setEmail(restaurant.getEmail());
         restaurantResponse.setPhoneNumber(restaurant.getPhoneNumber());
         restaurantResponse.setDietType(restaurant.getDietType());
