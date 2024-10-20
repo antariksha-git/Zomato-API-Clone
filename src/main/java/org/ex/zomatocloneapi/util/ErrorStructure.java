6128100e279f6e3c466866bead44505bccc3c015
antariksha-git <heyantariksha@gmail.com>
2024-10-20 19:11:07 +0530

    Added proper response & error structures

---
+++ src/main/java/org/ex/zomatocloneapi/util/ErrorStructure.java
@@ -0,0 +1,24 @@
+package org.ex.zomatocloneapi.util;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+public class ErrorStructure<T> {
+
+    private int statusCode;
+    private String message;
+    private T rootCause;
+
+    public static <T> ErrorStructure<T> getErrorStructure(HttpStatus statusCode, T rootCause, String message) {
+        ErrorStructure<T> errorStructure = new ErrorStructure<>();
+        errorStructure.setStatusCode(statusCode.value());
+        errorStructure.setRootCause(rootCause);
+        errorStructure.setMessage(message);
+
+        return errorStructure;
+    }
+}
+
