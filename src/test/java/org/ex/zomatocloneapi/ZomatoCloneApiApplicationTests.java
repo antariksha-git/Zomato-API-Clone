626276592cbfc9b5797edaf8bf1c1fe3a2f32e62
antariksha-git <heyantariksha@gmail.com>
2024-10-20 18:32:26 +0530

    Initial commit

---
+++ src/test/java/org/ex/zomatocloneapi/ZomatoCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ex.zomatocloneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoCloneApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
