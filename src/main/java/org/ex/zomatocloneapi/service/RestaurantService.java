ff4d699f9c967f1ec1ccc0ffd2eaa60743e3a441
antariksha-git <heyantariksha@gmail.com>
2024-10-20 19:54:32 +0530

    Created service class with addRestaurant method

---
+++ src/main/java/org/ex/zomatocloneapi/service/RestaurantService.java
@@ -0,0 +1,22 @@
+package org.ex.zomatocloneapi.service;
+
+import lombok.AllArgsConstructor;
+import org.ex.zomatocloneapi.entity.Restaurant;
+import org.ex.zomatocloneapi.mapper.RestaurantMapper;
+import org.ex.zomatocloneapi.repository.RestaurantRepository;
+import org.ex.zomatocloneapi.requestdto.RestaurantRequest;
+import org.ex.zomatocloneapi.responsedtao.RestaurantResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+
+    public RestaurantResponse addRestaurant(RestaurantRequest restaurantRequest) {
+        return RestaurantMapper
+                .mapToRestaurantResponse(restaurantRepository
+                        .save(RestaurantMapper.mapToRestaurant(restaurantRequest, new Restaurant())));
+    }
+}
