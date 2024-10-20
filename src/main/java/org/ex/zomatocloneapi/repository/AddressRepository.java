04b825784bc01e3170c2cbc24b8a556f8906de53
antariksha-git <heyantariksha@gmail.com>
2024-10-21 00:25:12 +0530

    Added AddressRepository

---
+++ src/main/java/org/ex/zomatocloneapi/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package org.ex.zomatocloneapi.repository;
+
+import org.ex.zomatocloneapi.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
