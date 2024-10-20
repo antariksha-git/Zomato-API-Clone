15344eadd3ddfefa35e6988c57170aca89591c90
antariksha-git <heyantariksha@gmail.com>
2024-10-21 00:48:23 +0530

    Refactored method name and to accept restaurant id as a parameter

--- src/main/java/org/ex/zomatocloneapi/service/AddressService.java
+++ src/main/java/org/ex/zomatocloneapi/service/AddressService.java
@@ -2,8 +2,10 @@ package org.ex.zomatocloneapi.service;
 
 import lombok.AllArgsConstructor;
 import org.ex.zomatocloneapi.entity.Address;
+import org.ex.zomatocloneapi.entity.Restaurant;
 import org.ex.zomatocloneapi.mapper.AddressMapper;
 import org.ex.zomatocloneapi.repository.AddressRepository;
+import org.ex.zomatocloneapi.repository.RestaurantRepository;
 import org.ex.zomatocloneapi.requestdto.AddressRequest;
 import org.ex.zomatocloneapi.responsedtao.AddressResponse;
 import org.springframework.stereotype.Service;
@@ -13,10 +15,15 @@ import org.springframework.stereotype.Service;
 public class AddressService {
 
     private final AddressRepository addressRepository;
+    private final RestaurantRepository restaurantRepository;
 
-    public AddressResponse addAddress(AddressRequest addressRequest) {
-        return AddressMapper
-                .mapToAddressResponse(addressRepository
-                        .save(AddressMapper.mapToAddress(addressRequest, new Address())));
+    public AddressResponse addAddressToRestaurant(AddressRequest addressRequest, String restaurantId) {
+        return restaurantRepository.findById(restaurantId)
+                .map(r -> {
+                    r.setAddress(AddressMapper.mapToAddress(addressRequest, new Address()));
+                    restaurantRepository.save(r);
+                    return AddressMapper.mapToAddressResponse(r.getAddress());
+                })
+                .orElseThrow();
     }
 }
