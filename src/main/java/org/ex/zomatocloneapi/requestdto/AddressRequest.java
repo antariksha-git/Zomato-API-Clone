ac2bb790c755f46b3f6b8f8738accf3c35a76031
antariksha-git <heyantariksha@gmail.com>
2024-10-21 00:08:48 +0530

    Created AddressRequest with fields

---
+++ src/main/java/org/ex/zomatocloneapi/requestdto/AddressRequest.java
@@ -0,0 +1,18 @@
+package org.ex.zomatocloneapi.requestdto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddressRequest {
+    private String addressLine1;
+    private String addressLine2;
+    private String landmark;
+    private String area;
+    private String city;
+    private String state;
+    private String pinCode;
+    private double latitude;
+    private double longitude;
+}
