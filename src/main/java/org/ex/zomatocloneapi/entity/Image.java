5b02bc7f9bdaf5f8d6f29540e51cda4eccbe227d
antariksha-git <heyantariksha@gmail.com>
2024-10-21 15:01:59 +0530

    Created image entity with fields

--- src/main/java/org/ex/zomatocloneapi/entity/Image.java
+++ src/main/java/org/ex/zomatocloneapi/entity/Image.java
@@ -1,9 +1,6 @@
 package org.ex.zomatocloneapi.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +11,7 @@ import lombok.Setter;
 @Getter
 public class Image {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "image_id")
     private int imageId;
 
