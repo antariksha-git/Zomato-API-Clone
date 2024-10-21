6a777e4a12ed35b93e0c75362e2315b7232f219c
antariksha-git <heyantariksha@gmail.com>
2024-10-21 15:05:24 +0530

    Added method to upload restaurant's logo

--- src/main/java/org/ex/zomatocloneapi/service/RestaurantService.java
+++ src/main/java/org/ex/zomatocloneapi/service/RestaurantService.java
@@ -1,6 +1,7 @@
 package org.ex.zomatocloneapi.service;
 
 import lombok.AllArgsConstructor;
+import org.ex.zomatocloneapi.entity.Image;
 import org.ex.zomatocloneapi.entity.Restaurant;
 import org.ex.zomatocloneapi.exception.RestaurantNotFoundByIdException;
 import org.ex.zomatocloneapi.exception.RestaurantNotFoundByIdException;
@@ -9,12 +10,14 @@ import org.ex.zomatocloneapi.repository.RestaurantRepository;
 import org.ex.zomatocloneapi.requestdto.RestaurantRequest;
 import org.ex.zomatocloneapi.responsedtao.RestaurantResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @AllArgsConstructor
 public class RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
+    private final ImageService imageService;
 
     public RestaurantResponse addRestaurant(RestaurantRequest restaurantRequest) {
         return RestaurantMapper
@@ -38,5 +41,19 @@ public class RestaurantService {
                 })
                 .orElseThrow(() -> new RestaurantNotFoundByIdException("Restaurant not found"));
 
+    }
+
+    public String uploadRestaurantLogo(String restaurantId, MultipartFile file) {
+        String imageUrl = imageService.uploadRestaurantLogo(file);
+        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new RuntimeException("Restaurant not found"));
+
+        Image image = new Image();
+        image.setUrl(imageUrl);
+
+        restaurant.setImage(image);
+        restaurantRepository.save(restaurant);
+
+        return imageUrl;
     }
 }
