be61ae114bf04740aba1992f3ed0ec1f6326a6eb
antariksha-git <heyantariksha@gmail.com>
2024-10-22 15:43:00 +0530

    Created mehtod to addCuisineToRestaurant

--- src/main/java/org/ex/zomatocloneapi/service/RestaurantService.java
+++ src/main/java/org/ex/zomatocloneapi/service/RestaurantService.java
@@ -1,16 +1,24 @@
 package org.ex.zomatocloneapi.service;
 
 import lombok.AllArgsConstructor;
+import org.ex.zomatocloneapi.entity.Cuisine;
 import org.ex.zomatocloneapi.entity.Image;
 import org.ex.zomatocloneapi.entity.Restaurant;
 import org.ex.zomatocloneapi.exception.RestaurantNotFoundByIdException;
 import org.ex.zomatocloneapi.exception.RestaurantNotFoundByIdException;
+import org.ex.zomatocloneapi.mapper.CuisineMapper;
 import org.ex.zomatocloneapi.mapper.RestaurantMapper;
 import org.ex.zomatocloneapi.repository.RestaurantRepository;
+import org.ex.zomatocloneapi.requestdto.CuisineRequest;
 import org.ex.zomatocloneapi.requestdto.RestaurantRequest;
+import org.ex.zomatocloneapi.responsedtao.CuisineResponse;
 import org.ex.zomatocloneapi.responsedtao.RestaurantResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -18,6 +26,7 @@ public class RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
     private final ImageService imageService;
+    private final CuisineService cuisineService;
 
     public RestaurantResponse addRestaurant(RestaurantRequest restaurantRequest) {
         return RestaurantMapper
@@ -55,5 +64,21 @@ public class RestaurantService {
         restaurantRepository.save(restaurant);
 
         return imageUrl;
+    }
+
+    public List<CuisineResponse> addCuisineToRestaurant(String restaurantId, List<CuisineRequest> cuisineRequest) {
+        Set<Cuisine> cuisines = cuisineRequest
+                .stream()
+                .map(c -> CuisineMapper.mapToCuisine(c, new Cuisine()))
+                .map(c -> cuisineService.findOrCreateCuisineByName(c.getTitle()))
+                .collect(Collectors.toSet());
+
+        return restaurantRepository.findById(restaurantId)
+                .map(r -> {
+                    r.setCuisines(cuisines);
+                    restaurantRepository.save(r);
+                    return r.getCuisines().stream().map(CuisineMapper::mapToCuisineResponse).toList();
+                })
+                .orElseThrow(() -> new RestaurantNotFoundByIdException("Restaurant not found by the given id"));
     }
 }
