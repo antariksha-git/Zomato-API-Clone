592165e131936fa041fccb824e1b2d0e17ee8ed3
antariksha-git <heyantariksha@gmail.com>
2024-10-22 17:17:24 +0530

    Created MenuCategoryRepository

---
+++ src/main/java/org/ex/zomatocloneapi/repository/MenuCategoryRepository.java
@@ -0,0 +1,7 @@
+package org.ex.zomatocloneapi.repository;
+
+import org.ex.zomatocloneapi.entity.MenuCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuCategoryRepository extends JpaRepository<MenuCategory, Long> {
+}
