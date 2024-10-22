5d5c64658e3c5c15fb1d1997d450de54b66423e7
antariksha-git <heyantariksha@gmail.com>
2024-10-22 17:14:32 +0530

    Added missing lombok's getter and setter

--- src/main/java/org/ex/zomatocloneapi/requestdto/MenuCategoryRequest.java
+++ src/main/java/org/ex/zomatocloneapi/requestdto/MenuCategoryRequest.java
@@ -1,5 +1,10 @@
 package org.ex.zomatocloneapi.requestdto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MenuCategoryRequest {
     private String title;
 }
--- src/main/java/org/ex/zomatocloneapi/responsedtao/MenuCategoryResponse.java
+++ src/main/java/org/ex/zomatocloneapi/responsedtao/MenuCategoryResponse.java
@@ -1,5 +1,10 @@
 package org.ex.zomatocloneapi.responsedtao;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MenuCategoryResponse {
     private long categoryId;
     private String title;
